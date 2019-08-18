e032e58d09b882a1725781bdaa9d8cc80c09ffe9
João Victor Ferreira <30957280+joaojvf@users.noreply.github.com>
2019-08-18 20:14:42 -0300

    Tela principal concluida

---
+++ src/screens/Agenda.js
@@ -0,0 +1,165 @@
+import React, {Component} from 'react'
+import {StyleSheet, Text, View, ImageBackground, FlatList,  TouchableOpacity, Platform} from 'react-native'
+import moment from 'moment'
+import 'moment/locale/pt-br'
+import todayImage from '../../assets/imgs/today.jpg'
+import commonStyles from '../commonStyles'
+import Task from '../components/Task';
+import Icon from    'react-native-vector-icons/FontAwesome';
+
+export default class Agenda extends Component {
+
+    state = {
+        tasks:[
+            { id: Math.random(),
+            desc: 'Comprar curso de react-native',
+            estimateAt: new Date (), doneAt: new Date() 
+            },
+            { id: Math.random(),
+                desc: 'Terminar o curso',
+                estimateAt: new Date (), doneAt: null 
+            },
+            { id: Math.random(),
+            desc: 'Comprar curso de react-native',
+            estimateAt: new Date (), doneAt: new Date() 
+            },
+            { id: Math.random(),
+                desc: 'Terminar o curso',
+                estimateAt: new Date (), doneAt: null 
+            },
+            { id: Math.random(),
+            desc: 'Comprar curso de react-native',
+            estimateAt: new Date (), doneAt: new Date() 
+            },
+            { id: Math.random(),
+                desc: 'Terminar o curso',
+                estimateAt: new Date (), doneAt: null 
+            },
+            { id: Math.random(),
+            desc: 'Comprar curso de react-native',
+            estimateAt: new Date (), doneAt: new Date() 
+            },
+            { id: Math.random(),
+                desc: 'Terminar o curso',
+                estimateAt: new Date (), doneAt: null 
+            },
+            { id: Math.random(),
+            desc: 'Comprar curso de react-native',
+            estimateAt: new Date (), doneAt: new Date() 
+            },
+            { id: Math.random(),
+                desc: 'Terminar o curso',
+                estimateAt: new Date (), doneAt: null 
+            },
+            { id: Math.random(),
+            desc: 'Comprar curso de react-native',
+            estimateAt: new Date (), doneAt: new Date() 
+            },
+            { id: Math.random(),
+                desc: 'Terminar o curso',
+                estimateAt: new Date (), doneAt: null 
+            },
+        ],
+        visibleTasks: [],
+        showDoneTasks: true,
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null;
+
+        if (this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        }
+        else {
+            const pending = task => task.doneAt === null;
+            visibleTasks = this.state.tasks.filter(pending);
+        }
+
+        this.setState({visibleTasks});
+    }
+
+    togggleFilter  = () => {
+        //Altera o estado do atributo para o valor oposto do atual e chama a função filter tasks em seguida
+        this.setState ({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks);
+        
+    }
+
+    // funcao utilizada para setar como completada ou não uma task
+    toggleTask = id => {
+        const tasks = this.state.tasks.map(task => {
+            if (task.id === id) {
+                task = {...task};
+                task.doneAt = task.doneAt ? null : new Date ();
+            }
+            return task;
+        });
+
+        this.setState({tasks}, this.filterTasks);
+    }
+
+    componentDidMount = () => {
+        this.filterTasks();
+    }
+
+    render () {
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage} style={styles.background}>
+                    <View style = {styles.iconBar}>
+                        <TouchableOpacity onPress = {this.togggleFilter}>
+                            <Icon name = {this.state.showDoneTasks ? 'eye' : 'eye-slash'} size={20} color = {commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subtitle}>
+                            {moment().locale('pt-br').format('ddd, D [de] MMM')}
+                        </Text>
+                    </View>
+                </ImageBackground>
+                <View style = {styles.taskContainer}>
+                   <FlatList data = {this.state.visibleTasks}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={({item}) => 
+                    <Task {...item} toggleTask={this.toggleTask}/>}
+                    />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,        
+    },
+    background: {
+        flex: 3,
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        marginLeft: 20,
+        marginBottom: 10,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        fontSize: 20,
+        color: commonStyles.colors.secondary,
+        marginLeft: 20,
+        marginBottom: 10,
+    },
+    taskContainer: {
+        flex: 7
+    },
+    iconBar: {
+        marginTop: Platform.OS === 'ios' ? 30 : 10,
+        marginHorizontal: 20,
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    }
+});
