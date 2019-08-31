71a7f0791dcb741000c158a51362dfcdb3e3cf00
João Victor Ferreira <30957280+joaojvf@users.noreply.github.com>
2019-08-31 20:34:42 -0300

    Adicionado filtro de data na agenda

--- tasks/src/commonStyles.js
+++ tasks/src/commonStyles.js
@@ -1,10 +1,13 @@
 export default {
     fontFamily: 'Lato',
     colors: {
+        default: '#1631be',
         today: '#b13b44',
-        secondary: '#FFF',
+        tomorrow: '#c974as2e',
+        week: '#15721e',
+        month: '#1631be',
         mainText: '#222',
         subText: '#555',
-        default: '#1631be'
+        secondary: '#FFF'
     }
 }
--- tasks/src/screens/Agenda.js
+++ tasks/src/screens/Agenda.js
@@ -11,13 +11,18 @@ import {
 import moment from 'moment'
 import axios from 'axios';
 import 'moment/locale/pt-br'
+
 import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
+
 import commonStyles from '../commonStyles'
 import Task from '../components/Task';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import ActionButton from 'react-native-action-button';
 import AddTask from './AddTask';
-import {server, showError} from '../common'
+import { server, showError } from '../common'
 
 
 export default class Agenda extends Component {
@@ -31,23 +36,24 @@ export default class Agenda extends Component {
 
     loadTasks = async () => {
         try {
-            const maxDate = moment().format('YYYY-MM-DD 23:59');
-            const res = await axios.get(`${server}/tasks?date = ${maxDate}`);
-
-            this.setState({tasks: res.data}, this.filterTasks)
+            const maxDate = moment()
+                .add({ days: this.props.daysAhead })
+                .format('YYYY-MM-DD 23:59')
+            const res = await axios.get(`${server}/tasks?date=${maxDate}`)
+            this.setState({ tasks: res.data }, this.filterTasks)
         } catch (err) {
-            showError(err);
+            showError(err)
         }
     }
 
     addTask = async task => {
         try {
             await axios.post(`${server}/tasks`, {
-                desc:task.desc,
+                desc: task.desc,
                 estimateAt: task.date
             });
 
-            this.setState({showAddTask: false}, this.loadTasks)
+            this.setState({ showAddTask: false }, this.loadTasks)
 
         } catch (err) {
             showError(err);
@@ -96,24 +102,49 @@ export default class Agenda extends Component {
             await axios.delete(`${server}/tasks/${id}`)
             await this.loadTasks();
         } catch (err) {
-            
+
         }
     }
 
     render() {
+        let styleColor = null;
+        let image = null;
+
+        switch (this.props.daysAhead) {
+            case 0:
+                styleColor = commonStyles.colors.today;
+                image = todayImage;
+                break;
+            case 1:
+                styleColor = commonStyles.colors.tomorrow;
+                image = tomorrowImage;
+                break;
+            case 7:
+                styleColor = commonStyles.colors.week;
+                image = weekImage;
+                break;
+            default:
+                styleColor = commonStyles.colors.month;
+                image = monthImage;
+                break;
+        }
+
         return (
             <View style={styles.container}>
                 <AddTask isVisible={this.state.showAddTask}
                     onSave={this.addTask}
                     onCancel={() => this.setState({ showAddTask: false })} />
-                <ImageBackground source={todayImage} style={styles.background}>
+                <ImageBackground source={image} style={styles.background}>
                     <View style={styles.iconBar}>
+                        <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+                            <Icon name ='bars' size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
                         <TouchableOpacity onPress={this.togggleFilter}>
                             <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} size={20} color={commonStyles.colors.secondary} />
                         </TouchableOpacity>
                     </View>
                     <View style={styles.titleBar}>
-                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.title}>{this.props.title}</Text>
                         <Text style={styles.subtitle}>
                             {moment().locale('pt-br').format('ddd, D [de] MMM')}
                         </Text>
@@ -127,7 +158,7 @@ export default class Agenda extends Component {
                                 onDelete={this.deleteTask} />}
                     />
                 </View>
-                <ActionButton buttonColor={commonStyles.colors.today}
+                <ActionButton buttonColor={styleColor}
                     onPress={() => { this.setState({ showAddTask: true }) }} />
             </View>
         )
@@ -165,6 +196,6 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === 'ios' ? 30 : 10,
         marginHorizontal: 20,
         flexDirection: 'row',
-        justifyContent: 'flex-end'
+        justifyContent: 'space-between'
     }
 });
